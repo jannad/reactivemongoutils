7088fe3adbf3797aaf80844b2c80d9a8f1a12ff3
Jan Nad <jan.nad@finfactory.com>
2016-04-03 20:56:09 +0200

    Added path interpolation. Minor fixes

---
+++ src/main/scala/com/github/jannad/reactivemongoutils/BSONPathInterpolation.scala
@@ -0,0 +1,48 @@
+package com.github.jannad.reactivemongoutils
+
+import reactivemongo.bson.BSONValue
+
+/**
+  * @author jannad
+  *
+  * Custom string interpolation for BSON document paths.
+  *
+  * Usage: path"fieldA.fieldB.${fieldC}.array1.10" produces a function that can be applied to BSONValue
+  */
+object BSONPathInterpolation {
+	sealed trait BSONPathPart
+	sealed case class BSONFieldName(name: String) extends BSONPathPart
+	sealed case class BSONArrayIndex(idx: Int) extends BSONPathPart
+
+	implicit class BSONPathHelper(val sc: StringContext) extends AnyVal {
+
+		def path(args: Any*): BSONValue => ExtendedBSONDocument = {
+			def interleave(args: List[Any], parts: List[String]): List[Any] = {
+				(args, parts) match {
+					case (Nil, p::Nil) => p::Nil
+					case (a1::xa, p1::xp) => p1::a1::interleave(xa, xp)
+				}
+			}
+
+			val pathStr = interleave(args.toList, sc.parts.toList).map(_.toString)
+
+			val Num = "([0-9])+".r
+			val pathParts: Array[BSONPathPart] = pathStr.mkString.split("\\.").map(_ match {
+				case Num(n) => BSONArrayIndex(n.toInt)
+				case p => BSONFieldName(p)
+			})
+
+			def runPath(path: Array[BSONPathPart])(b: BSONValue) = {
+				val root = ExtendedBSONDocument(Some(b))
+				path.foldLeft(root)((doc, pathPart) => {
+					pathPart match {
+						case BSONFieldName(s) => doc \ s
+						case BSONArrayIndex(n) => doc \ n
+					}
+				})
+			}
+
+			runPath(pathParts)
+		}
+	}
+}
--- src/main/scala/com/github/jannad/reactivemongoutils/ExtendedBSONDocument.scala
+++ src/main/scala/com/github/jannad/reactivemongoutils/ExtendedBSONDocument.scala
@@ -9,6 +9,10 @@ import reactivemongo.bson.{BSONArray, BSONDateTime, BSONDocument, BSONDouble, BS
   * @author jannad
   */
 object Implicits {
+
+	/**
+	  * Converts BSONValue to human-readable string
+	  */
 	implicit val bsonValShow = new Show[BSONValue] {
 		override def show(v: BSONValue) = {
 			def f(v: BSONValue): String = v match {
@@ -84,17 +88,24 @@ object Demo {
 	val doc = BSONDocument(
 		"subdoc1" -> BSONDocument("intField" -> 123),
 		"id" -> BSONObjectID("507f1f77bcf86cd799439011"),
-		"subdoc2" -> BSONDocument("subdoc2.1" -> BSONDocument("strField" -> "xxxx"), "arr" -> BSONArray(1, 2, 3, 4, 5)
+		"subdoc2" -> BSONDocument("subdoc2_1" -> BSONDocument("strField" -> "xxxx"), "arr" -> BSONArray(1, 2, 3, 4, 5)
 		)
 	)
 
 	def main(args: Array[String]) {
-		println((doc \ "subdoc2" \ "subdoc2.1" \ "strField").as[String])
+		println((doc \ "subdoc2" \ "subdoc2_1" \ "strField").as[String])
 		println((doc \ "subdoc1" \ "intField").as[Int])
 		println((doc \ "subdoc2" \ "arr" \ 1).as[Int])
-		println(doc \ "subdoc2" \ "arr" \ 1)
+		println((doc \ "abc" \ "def" \ 1).as[Double])
 
 		import cats.syntax.show._
 		println(doc.show)
+
+		import BSONPathInterpolation._
+		val index = 3
+		val arrField = "arr"
+		println(path"subdoc2.subdoc2_1.strField"(doc).as[String])
+		println(path"subdoc2.$arrField.$index"(doc).as[Int])
+
 	}
 }
